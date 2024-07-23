9ecc4f539904b4480d9e254534e64ac9ceaa517c
harika <charan02ch@gmail.com>
2024-07-23 19:08:20 +0530

    M

--- s3-state/main.tf
+++ s3-state/main.tf
@@ -18,10 +18,13 @@ resource "aws_instance" "web" {
   
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
 
 resource "aws_s3_bucket" "my_buk" {
   bucket = "my-tf-test-bucket"
-  region         = "us-east-1"
+  
 
   tags = {
     Name        = "My bucket"
