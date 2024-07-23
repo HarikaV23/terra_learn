bae6e9c1765fd15429c3cec095358a7eaf8e3d2b
harika <charan02ch@gmail.com>
2024-07-23 14:20:01 +0530

    M

--- sample/ec2.tf
+++ sample/ec2.tf
@@ -2,5 +2,8 @@ resource "aws_instance" "web" {
     ami = "ami-0b4f379183e5706b9"
     instance_type = "t2.nano"
     vpc_security_group_ids = ["sg-0f2cd6b7ac0f58e0e"]
+    tags = {
+        Name ="tet"
+    }
   
 }
