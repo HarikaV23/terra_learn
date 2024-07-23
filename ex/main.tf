24a8c1f0741c5f18adbf03e9a2862f594ea1cfd1
harika <charan02ch@gmail.com>
2024-07-23 21:04:38 +0530

    M

--- ex/main.tf
+++ ex/main.tf
@@ -17,31 +17,23 @@ resource "aws_instance" "web" {
 }
 
 
-resource "aws_route53_record" "www" {
-  for_each = var.instances
-  zone_id = each.value["zoneid"]
-  name    = var.instances.private_ip
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.web.each.value["name"].private_ip]
-}
 
 variable "instances" {
     default = {
         frontend = {
             name = "frontend"
             type = "t2.micro"
-            zoneid ="Z03841342MEYUTSA07CRA"
+            
         }
         user = {
             name = "user"
             type = "t2.micro"
-            zoneid ="Z03841342MEYUTSA07CRA"
+           
         }
         catalouge = {
             name = "catalouge"
             type = "t2.micro"
-            zoneid ="Z03841342MEYUTSA07CRA"
+           
         }
     }
   
