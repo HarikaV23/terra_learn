dc5b7325d0b1d2c45786f55be25bb88866eb6868
harika <charan02ch@gmail.com>
2024-07-23 18:59:58 +0530

    M

---
+++ s3-state/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-b71"
+    key            = "s3-state/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  
+  }
+}
+
+resource "aws_instance" "web" {
+    ami = "ami-0b4f379183e5706b9"
+    instance_type = "t2.nano"
+    vpc_security_group_ids = ["sg-0f2cd6b7ac0f58e0e"]
+    tags = {
+        Name ="test"
+    }
+  
+}
