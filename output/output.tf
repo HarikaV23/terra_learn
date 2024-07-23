479814a83c5743279e4462dab7375c53997e7c29
harika <charan02ch@gmail.com>
2024-07-23 14:34:50 +0530

    M

--- output/output.tf
+++ output/output.tf
@@ -1,10 +1,12 @@
-data "aws_instance" "web" {
-    ami = "ami-0b4f379183e5706b9"
+data "aws_ami" "web" {
+    most_recent = true
+    name_regex = "Centos-8-DevOps-Practice"
+    owners = ["973714476881"]
   
 }
 
 
 output "sample" {
-    value  = aws_instance.web.instance_type
+    value  = data.aws_ami.web.image_id
   
 }
