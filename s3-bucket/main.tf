b24e8f03c4789657f8ca7160576de88dd649966a
harika <charan02ch@gmail.com>
2024-07-23 19:20:26 +0530

    M

---
+++ s3-bucket/main.tf
@@ -0,0 +1,12 @@
+
+resource "aws_s3_bucket" "bucket56789" {
+  bucket = "bucket9033j2hs"
+    tags = {
+      Name        = "MyS3Bucket"
+      Environment = "Production"
+    }
+}
+resource "aws_s3_bucket_acl" "ACL" {
+  bucket = aws_s3_bucket.ACL.id
+  acl    = "private"
+}
