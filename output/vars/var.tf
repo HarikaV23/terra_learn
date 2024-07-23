72e3bd1cffd40d5ac4b479efa105cb98537c19e4
harika <charan02ch@gmail.com>
2024-07-23 14:49:01 +0530

    M

---
+++ output/vars/var.tf
@@ -0,0 +1,23 @@
+variable "sample" {
+    default = "test"
+  
+}
+
+output "sample" {
+    value = var.sample
+  
+}
+
+variable "sample1" {}
+
+output "sample1" {
+    value = var.sample1
+  
+}
+
+variable "cli" {}
+
+output "cli" {
+    value = var.cli
+  
+}
