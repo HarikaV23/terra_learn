7e9f3dd94a4f79d24d747f2f149d36c0aaccc0d8
harika <charan02ch@gmail.com>
2024-07-23 21:08:45 +0530

    M

--- ex/main.tf
+++ ex/main.tf
@@ -6,13 +6,20 @@ data "aws_ami" "web" {
 }
 
 resource "aws_instance" "web" {
-    for_each = var.instances
+   # for_each = var.instances
+    count = length(var.instance)
     ami = data.aws_ami.web.image_id
     instance_type = each.value["type"]
     vpc_security_group_ids = ["sg-0f2cd6b7ac0f58e0e"]
     tags = {
-        Name = each.value["name"]
+       # Name = each.value["name"]
+       Name = var.instance[count.index]
     }
+  
+}
+
+variable "instance" {
+    default = ["frontend", "user","catalouge"]
   
 }
 
